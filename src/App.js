3af358d74d83a2b3c92930ddd5790991433c4928
LacamJC <joaoramajo744@gmail.com>
2025-04-17 13:41:25 -0300

    Usuário: LacamJC| Data: 2025-04-17 10:08

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@
 import React from 'react';
 import './App.css';
 import TabelaDeDuasSemanas from './TabelaDeDuasSemanas';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import QuadroAviso from './components/widgets/QuadroAviso';
 function App() {
   
 
   return (
     <>
-      <TabelaDeDuasSemanas/>
+      {/* <TabelaDeDuasSemanas/> */}
+      <div className="row w-100 content">
+        <div className="col col-md-3 bg-sudccess side-bar p-0">
+          <div className='calendario bg-success'>
+            
+          </div>
+          <div className='avisos bgwarning d-flex justify-content-center align-items-center'>
+            <QuadroAviso/>
+          </div>
+        </div>
+        <div className="col col-md-9 overflow-scroll agenda p-0">
+          <TabelaDeDuasSemanas/>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/widgets/QuadroAviso.js
@@ -0,0 +1,14 @@
+
+const QuadroAviso = () => {
+    return (
+        <>
+
+            <ul class="list-group w-100 px-2">
+                <h2 className="text-center">Avisos gerais</h2>
+                <li class="list-group-item">Colaborador x Entrara de ferias dia 12/04</li>
+            </ul>
+        </>
+    )
+}
+
+export default QuadroAviso
